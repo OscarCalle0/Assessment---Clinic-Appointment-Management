b105b37ae637cf68839acb7aa8e3e92a90e9c9a5
oscarcalle <oscarcalle0@gmail.com>
2024-11-15 09:57:20 -0500

    feat: create connexion data base

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+using Appointment_Management.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hotel.Data;
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Appointment> Appointments { get; set; }
+    public DbSet<AvailabilitySchedule> AvailabilitySchedules { get; set; }
+    public DbSet<Doctor> Doctors { get; set; }
+    public DbSet<Log> Logs { get; set; }
+    public DbSet<Patient> Patients { get; set; }
+    public DbSet<Specialty> Specialties { get; set; }
+    public DbSet<User> Users { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // call of the sedders
+       
+    }
+}
